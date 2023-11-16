351a26c4c63cd2d0e654862bb1557f4671355df6
Moisés S. Sutil <moisessutil222@gmail.com>
2023-11-16 17:18:46 -0300

    novos ex

--- python-POO/aula141/log.py
+++ python-POO/aula141/log.py
@@ -1,4 +1,7 @@
 # Abstração
+from pathlib import Path
+
+LOG_FILE = Path(__file__).parent / 'log.txt'
 
 class Log:
     def _log(self, msg):
@@ -13,7 +16,11 @@ class Log:
 
 class LogFileMixin(Log):
     def _log(self, msg):
-        print(msg)
+        msg_formatada = f'{msg} ({self.__class__.__name__})'
+        print('Salvando no log:', msg_formatada)
+        with open(LOG_FILE, 'a') as arquivo:
+            arquivo.write(msg_formatada)
+            arquivo.write('\n')
 
 
 class LogPrintMixin(Log):
@@ -22,6 +29,9 @@ class LogPrintMixin(Log):
 
 
 if __name__ == '__main__':
-    l = LogPrintMixin()
-    l.log_error('qualquer coisa')
-    l.log_success('Que legal')
+    lp = LogPrintMixin()
+    lp.log_error('qualquer coisa')
+    lp.log_success('Que legal')
+    lf = LogFileMixin()
+    lf.log_error('qualquer coisa')
+    lf.log_success('Que legal')
